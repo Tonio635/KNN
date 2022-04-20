d441df36926152e9e941f1619d6ec0d8f94ae617
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-04-21 01:48:23 +0200

    Commenti file KNN.java e DiscreteAttribute.java

--- src/DiscreteAttribute.java
+++ src/DiscreteAttribute.java
@@ -1,6 +1,16 @@
+/**
+ * Estende la classe Attribute e rappresenta un attributo discreto
+ */
 public class DiscreteAttribute extends Attribute{
 
-    public DiscreteAttribute(String name, int index){       //Invoca il costruttore della super-classe
+    /**
+     * Costruttore della classe
+     * Invoca il costruttore della super-classe
+     * 
+     * @param name indica il nome simbolico dell'attributo con cui avvalorare il valore del membro della super-classe name
+     * @param index indica l'identificativo numerico dell'attributo con cui avvalorare il valore del membro della super-classe index
+     */
+    public DiscreteAttribute(String name, int index){
         super(name, index);
     }
 }
--- src/KNN.java
+++ src/KNN.java
@@ -1,16 +1,38 @@
+/**
+ * Modella il miner
+ */
 public class KNN {
+    // Modella il training set
     private Data data;
 
+    /**
+     * Costruttore della classe
+     * Avvalora il training set
+     * 
+     * @param trainingSet indica il training set con cui avvalorare l'attributo
+     */
     public KNN(Data trainingSet){
         data = trainingSet;
     }
 
+    /**
+     * Predice il valore target dell’esempio passato come parametro
+     * 
+     * @param e indica l'esempio di cui predire il valore target
+     * @param k indica la vicinanza massima degli esempi da considerare
+     * @return predizione del valore target dell'esempio passato come parametro
+     */
     public double predict(Example e, int k){
         double predict;
         predict = data.avgClosest(e, k);
         return predict;
     }
 
+    /**
+     * Restituisce i valori del miner
+     * 
+     * @return stringa contentente i valori del miner
+     */
     public String toString(){
         return data.toString();
     }
