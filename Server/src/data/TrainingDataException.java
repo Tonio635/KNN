6699c7508f490005e42e3186eaf6aeb1c433ed9a
D0men1c0 <d.lacavalla2@studenti.uniba.it>
2022-06-23 00:02:38 +0200

    Commenti e visibilità TrainingDataException

--- Server/src/data/TrainingDataException.java
+++ Server/src/data/TrainingDataException.java
@@ -1,16 +1,20 @@
 package data;
 
 /**
- * Eccezione che controlla che il TrainingSet sia stato acquisito in maniera
+ * Eccezione controllata che controlla che il TrainingSet sia stato acquisito in maniera
  * corretta
  */
 public class TrainingDataException extends Exception {
 
-    public TrainingDataException() {
-
-    }
-
-    public TrainingDataException(String message) {
+    /**
+	 * Costruttore di classe.
+	 * 
+	 * Richiama il costruttore della super-classe che permette di sollevare
+	 * un'eccezione con un messaggio passato come parametro.
+	 * 
+	 * @param msg Messaggio passato al momento del sollevamento dell'eccezione.
+	 */
+    TrainingDataException(String message) {
         super(message);
     }
 }
