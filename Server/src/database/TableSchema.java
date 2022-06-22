2596e325a38dc22f6521c701b979071c19ea5f71
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-22 23:18:28 +0200

    Aggiunta commenti + modifica nome al metodo target + aggiunto toString in TableSchema

--- Server/src/database/TableSchema.java
+++ Server/src/database/TableSchema.java
@@ -8,12 +8,34 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Classe per definire lo schema della tabella del database da usare per
+ * l'estrazione dei dati.
+ */
 public class TableSchema implements Iterable<Column> {
 
 	private List<Column> tableSchema = new ArrayList<Column>();
 	private Column target;
 	private String tableName;
 
+	/**
+	 * Costruttore di classe.
+	 * 
+	 * Serve per inizializzare il nome della tabella,
+	 * la colonna che definisce la variabile dipendente e una lista contentente
+	 * tutte le variabili indipendenti.
+	 * 
+	 * @param tableName Nome della tabella.
+	 * @param db        Oggetto contenente i dati della connessione al database.
+	 * @throws SQLException                      Eccezione che fornisce informazioni
+	 *                                           su errori di accesso al database o
+	 *                                           di altro genere.
+	 * @throws InsufficientColumnNumberException Eccezione sollevata nel caso in
+	 *                                           cui la tabella non dovesse avere
+	 *                                           variabili indipendenti o non
+	 *                                           dovesse avere la variabile
+	 *                                           dipendente.
+	 */
 	public TableSchema(String tableName, DbAccess db) throws SQLException, InsufficientColumnNumberException {
 		this.tableName = tableName;
 
@@ -46,21 +68,58 @@ public class TableSchema implements Iterable<Column> {
 			throw new InsufficientColumnNumberException("La tabella selezionata contiene meno di due colonne");
 	}
 
-	public Column target() {
+	/**
+	 * Metodo per l'accesso alla colonna della variabile dipendente.
+	 * 
+	 * @return Colonna della variabile dipendente.
+	 */
+	public Column getTarget() {
 		return target;
 	}
 
+	/**
+	 * Metodo per l'accesso al numero di variabili indipendenti presenti nella tabella.
+	 * 
+	 * @return Numero di variabili indipendenti.
+	 */
 	public int getNumberOfAttributes() {
 		return tableSchema.size();
 	}
 
+	/**
+	 * Metodo per l'accesso al nome della tabella del database.
+	 * 
+	 * @return Nome della tabella.
+	 */
 	String getTableName() {
 		return tableName;
 	}
 
+	/**
+	 * Metodo per l'accesso all'iteratore della lista contenente le colonne
+	 * delle variabili indipendenti.
+	 * 
+	 * @return Iteratore della lista tableSchema.
+	 */
 	@Override
 	public Iterator<Column> iterator() {
 		return tableSchema.iterator();
 	}
 
+	/**
+	 * Metodo per la trasformazione in stringa delle informazioni di un oggetto
+	 * di tipo TableSchema.
+	 * 
+	 * @return Stringa contentente le informazioni dell'oggetto TableSchema.
+	 */
+	public String toString() {
+		String result = "";
+
+		result = "Nome tabella: " + tableName + "\n\n";
+		for(Column column : tableSchema) {
+			result += column + "\n";
+		}
+
+		return result;
+	}
 }
