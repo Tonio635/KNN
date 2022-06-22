88650c40149d72986b172067e0054f6692d4c341
D0men1c0 <d.lacavalla2@studenti.uniba.it>
2022-06-22 23:42:32 +0200

    NoValueException
    E' l'eccezione che non so a che serve e ci ho cambiato le visibilità

--- Server/src/database/NoValueException.java
+++ Server/src/database/NoValueException.java
@@ -1,8 +1,19 @@
 package database;
-
-public class NoValueException extends Exception {
-
-	public NoValueException(String msg) {
+/**
+ * Eccezione controlata che viene sollevata nel caso in cui i
+ * valori inseriti nel database non sono corretti.
+ */
+class NoValueException extends Exception {
+	
+	/**
+	 * Costruttore di classe.
+	 * 
+	 * Richiama il costruttore della super-classe che permette di sollevare
+	 * un'eccezione con un messaggio passato come parametro.
+	 * 
+	 * @param msg Messaggio passato al momento del sollevamento dell'eccezione.
+	 */
+	NoValueException(String msg) {
 		super(msg);
 	}
 
