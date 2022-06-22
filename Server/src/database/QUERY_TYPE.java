27354c38397960256a905d8af044d71816b2f685
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-22 23:09:06 +0200

    Commenti QUERY_TYPE

--- Server/src/database/QUERY_TYPE.java
+++ Server/src/database/QUERY_TYPE.java
@@ -1,5 +1,9 @@
 package database;
 
+/**
+ * Enumerazione utilizzata per estrarre dal database 
+ * i valori di minimo e massimo delle variabili indipendenti continue.
+ */
 public enum QUERY_TYPE {
     MIN, MAX
 }
