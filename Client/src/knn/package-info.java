0d3ef7507424485393cb9a0eccffa18d4e0fb0c3
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-06-25 18:36:56 +0200

    aggiunta package-info nella parte client

---
+++ Client/src/knn/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package principale che contiene i vari package usati nel progetto e
+ * la classe Main che serve come punto di partenza dell'applicazione lato client.
+ */
+package knn;
---
+++ Client/src/knn/utility/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package contenente classi non propriamente inerenti al progetto ma utili al
+ * fine della sua realizzazione.
+ */
+package knn.utility;
