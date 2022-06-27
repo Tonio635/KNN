911414249ecad54eb364c8b7de046083f42e997b
Matteo Balice <matteobalice02@gmail.com>
2022-06-27 21:13:04 +0200

    PRIMO COMMIT SPRING

---
+++ KNN esteso/knn/src/test/java/knn/knn/KnnApplicationTests.java
@@ -0,0 +1,13 @@
+package knn.knn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
