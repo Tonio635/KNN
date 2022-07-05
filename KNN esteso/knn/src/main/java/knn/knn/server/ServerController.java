87f4744e7aa8b14e1723f9defbb3f03cf3317020
D0men1c0 <d.lacavalla2@studenti.uniba.it>
2022-07-06 00:30:07 +0200

    Non ci ho capito niente :)

--- KNN esteso/knn/src/main/java/knn/knn/server/ServerController.java
+++ KNN esteso/knn/src/main/java/knn/knn/server/ServerController.java
@@ -1,7 +1,17 @@
 package knn.knn.server;
 
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Classe server che gestisce le richieste del client
@@ -18,5 +28,10 @@ public class ServerController {
     public String index() {
         return "index";
     }
-
+    
+    @RequestMapping(value = "/domenico", method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<String> checkUsername(@RequestBody String str) {
+        return new ResponseEntity<String>(str, HttpStatus.OK);
+    }
 }
+
