3ce69b19d59598424b5d61171ba5dd64e741acd8
D0men1c0 <d.lacavalla2@studenti.uniba.it>
2022-07-06 00:30:54 +0200

    Tolti import inutili

--- KNN esteso/knn/src/main/java/knn/knn/server/ServerController.java
+++ KNN esteso/knn/src/main/java/knn/knn/server/ServerController.java
@@ -1,17 +1,12 @@
 package knn.knn.server;
 
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Classe server che gestisce le richieste del client
