37e0a95ae490286af465aa919b0491b2964f7866
Antonio Giuseppe Doronzo <antoniodoronzo7@gmail.com>
2022-07-19 18:09:56 +0200

    inserimento impostazioni javadoc e fix commenti progetto base

--- KNN base/Server/src/knn/example/ExampleSizeException.java
+++ KNN base/Server/src/knn/example/ExampleSizeException.java
@@ -5,6 +5,15 @@ package knn.example;
  * lo stesso numero di variabili indipendenti
  */
 class ExampleSizeException extends RuntimeException {
+
+	/**
+	 * Costruttore di classe.
+	 * 
+	 * Richiama il costruttore della super-classe che permette di sollevare
+	 * un'eccezione con un messaggio passato come parametro.
+	 * 
+	 * @param message Messaggio passato al momento del sollevamento dell'eccezione.
+	 */
 	ExampleSizeException(String message) {
 		super(message);
 	}
